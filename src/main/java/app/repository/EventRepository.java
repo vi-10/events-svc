79beed4dc21dd9c5855db4bfdaf14144b5bf6527
vi-10 <vaninailieva@gmail.com>
2026-08-06 18:56:18 +0300

    feat: add findByActiveTrue() method in EventRepository

--- src/main/java/app/repository/EventRepository.java
+++ src/main/java/app/repository/EventRepository.java
@@ -4,9 +4,10 @@ import app.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
-
+    Optional<Event> findByActiveTrue();
 }
