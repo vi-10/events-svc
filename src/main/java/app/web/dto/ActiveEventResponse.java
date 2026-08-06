2f4f204a70aab5662a0438179c8067256b4f3bd6
vi-10 <vaninailieva@gmail.com>
2026-08-06 18:52:38 +0300

    feat: add ActiveEventResponse

---
+++ src/main/java/app/web/dto/ActiveEventResponse.java
@@ -0,0 +1,26 @@
+package app.web.dto;
+
+import app.model.QuestType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActiveEventResponse {
+
+    private String title;
+    private String description;
+    private QuestType affectedQuestType;
+    private int bonusXp;
+    private int bonusGold;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+}
