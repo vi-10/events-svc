66e90702ff286b2c91d941cf5a6ced96c4b3f8da
vi-10 <vaninailieva@gmail.com>
2026-08-06 18:57:08 +0300

    feat: add getActiveEvent() in EventService

--- src/main/java/app/service/EventService.java
+++ src/main/java/app/service/EventService.java
@@ -1,6 +1,8 @@
 package app.service;
 
 import app.repository.EventRepository;
+import app.web.dto.ActiveEventResponse;
+import app.web.mapper.EventMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +10,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EventService {
 
-    private EventRepository eventRepository;
+    private final EventRepository eventRepository;
+
+    public ActiveEventResponse getActiveEvent() {
+
+        return eventRepository.findByActiveTrue()
+                .map(EventMapper::toActiveEventResponse)
+                .orElse(null);
+    }
 }
