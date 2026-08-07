11f53d5758fb6ab390b0c96d3ffc89c4d9b82570
vi-10 <vaninailieva@gmail.com>
2026-08-08 01:33:09 +0300

    feat: add toEventDTO method in EventMapper

--- src/main/java/app/web/mapper/EventMapper.java
+++ src/main/java/app/web/mapper/EventMapper.java
@@ -2,6 +2,7 @@ package app.web.mapper;
 
 import app.model.Event;
 import app.web.dto.ActiveEventResponse;
+import app.web.dto.EventDTO;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
@@ -17,4 +18,19 @@ public class EventMapper {
                 .end(event.getEnd())
                 .build();
     }
+
+    public static EventDTO toEventDTO (Event event){
+        return EventDTO.builder()
+                .id(event.getId())
+                .title(event.getTitle())
+                .description(event.getDescription())
+                .affectedQuestType(event.getAffectedQuestType())
+                .bonusXp(event.getBonusXp())
+                .bonusGold(event.getBonusGold())
+                .start(event.getStart())
+                .end(event.getEnd())
+                .build();
+    }
+
+
 }
