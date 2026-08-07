31688aab92c6825e36c0f159524df6e87eca27d1
vi-10 <vaninailieva@gmail.com>
2026-08-07 20:08:20 +0300

    feat: add createEvent method in EventController

--- src/main/java/app/web/EventController.java
+++ src/main/java/app/web/EventController.java
@@ -2,11 +2,14 @@ package app.web;
 
 import app.service.EventService;
 import app.web.dto.ActiveEventResponse;
+import app.web.dto.CreateEventRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/api/v1/event")
@@ -26,4 +29,15 @@ public class EventController {
 
         return ResponseEntity.ok(event);
     }
+
+    @PostMapping
+    public ResponseEntity<Void> createEvent(
+            @Valid @RequestBody CreateEventRequest request) {
+
+        eventService.createEvent(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
 }
