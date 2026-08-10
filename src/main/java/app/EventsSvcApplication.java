22a82d455e5607649920ea225e58d360d7eb31fb
vi-10 <vaninailieva@gmail.com>
2026-08-10 15:03:03 +0300

    feat: add @EnableScheduling and @EnableCaching in EventsSvcApplication

--- src/main/java/app/EventsSvcApplication.java
+++ src/main/java/app/EventsSvcApplication.java
@@ -2,8 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
 public class EventsSvcApplication {
 
 	public static void main(String[] args) {
