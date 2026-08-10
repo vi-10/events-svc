91bdd305dd126402b5f382cb9fc8e8ef8700c3a7
vi-10 <vaninailieva@gmail.com>
2026-08-10 15:02:07 +0300

    feat: add findByActiveTrueAndEndBefore(LocalDateTime time) in EventRepository

--- src/main/java/app/repository/EventRepository.java
+++ src/main/java/app/repository/EventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -41,4 +42,6 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
             LocalDateTime start,
             LocalDateTime end
     );
+
+    List<Event> findByActiveTrueAndEndBefore(LocalDateTime time);
 }
